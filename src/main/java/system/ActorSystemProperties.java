9a07108994f019539f58c97bdbcda39c4dd777dc
Puspendu Banerjee <Puspendu.Banerjee@gmail.com>
2016-09-13 13:16:57 -0500

    Simplified Property Handling

--- src/main/java/system/ActorSystemProperties.java
+++ src/main/java/system/ActorSystemProperties.java
@@ -20,52 +20,33 @@
 package system;
 
 import com.typesafe.config.Config;
+import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.env.PropertySource;
+
+import java.util.Properties;
 
 /**
  * Created by puspendu on 9/10/16.
  */
 
-public class ActorSystemProperties extends PropertySource {
+public class ActorSystemProperties extends PropertiesPropertySource {
     public static final String NAME = "actorSystemName";
     public static final String CONFIG = "config";
     private static final String propertySourceName = "actorSystemPropertySource";
-    private final String actorSystemName;
-    private Config config;
 
 
     private ActorSystemProperties(String name) {
-        this(name, null, null);
+        this(name, null, null, null);
     }
 
-    public ActorSystemProperties(String name, String actorSystemName, Config config) {
-        super(name);
-        this.actorSystemName = actorSystemName;
-        this.config = config;
+    public ActorSystemProperties(String name, String actorSystemName, Config config, Properties addtionalProperties) {
+        super(name,addtionalProperties);
+        source.put(NAME,actorSystemName);
+        source.put(CONFIG,config);
     }
 
-    public ActorSystemProperties(String actorSystemName, Config config) {
-        this(propertySourceName, actorSystemName, config);
-    }
-
-    public String getActorSystemName() {
-        return actorSystemName;
-    }
-
-
-    public void setConfig(Config config) {
-        this.config = config;
-    }
-
-    @Override
-    public Object getProperty(String s) {
-        switch (s) {
-            case CONFIG:
-                return config;
-            case NAME:
-                return actorSystemName;
-        }
-        return null;
+    public ActorSystemProperties(String actorSystemName, Config config,  Properties addtionalProperties) {
+        this(propertySourceName, actorSystemName, config, addtionalProperties);
     }
 
 }
--- src/main/scala/system/SpringContextActorSystemProvider.scala
+++ src/main/scala/system/SpringContextActorSystemProvider.scala
@@ -19,6 +19,8 @@
  */
 package system
 
+import java.util.Properties
+
 import akka.actor.ActorSystem
 import com.typesafe.config.{Config, ConfigFactory}
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
@@ -33,10 +35,12 @@ object SpringContextActorSystemProvider {
   val rootContext = new GenericApplicationContext()
   rootContext.refresh()
 
-  def create(name: String = "AkkaSpring", config: Config = ConfigFactory.load) = {
+  def create(actorSystemName: String = "AkkaSpring",
+             actorSystemConfig: Config = ConfigFactory.load,
+             additionalProperties: Properties=new Properties()) = {
     val ctx = new AnnotationConfigApplicationContext()
     ctx.setParent(rootContext)
-    val propertySource = new ActorSystemProperties(name, config);
+    val propertySource = new ActorSystemProperties(actorSystemName, actorSystemConfig,additionalProperties);
     ctx.getEnvironment.getPropertySources.addLast(propertySource)
     ctx.scan("system", "actors")
     ctx.refresh()
